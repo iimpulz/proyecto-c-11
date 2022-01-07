70b56f50694e4c5dcc0bf111f02ca3dec3755fba
iimpulz <96321241+iimpulz@users.noreply.github.com>
2022-01-06 18:19:13 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var weight = [35,38,42,45,43,34,36,41,48,32,50,60,77,89,42];
+var suma = 0;
+for (var i=0; i < weight.length; i++){
+  //suma=suma+weight[i];
+  if(weight[i]>=50){
+    console.log (weight[i]);
+  }
+}
+//console.log (suma);
+//var promedio = suma/weight.length;
+//console.log (promedio)
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
